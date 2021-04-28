8a576e093697fed07851833042b0b5aa5cae46e7
Adnan <adnanhaider530@gmail.com>
2021-04-28 12:29:49 +0500

    first commit

---
+++ app/code/Arslan/UiGrid/Plugin/Product.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Arslan\UiGrid\Plugin;
+
+class Product
+{
+    // public function afterGetPrice(\Magento\Catalog\Model\Product $subject, $result) {
+    //     return $result*2;
+    //  } 
+    // public function afterGetName( \Magento\Catalog\Model\Product $subject, $result)
+    // {
+
+    //  return "This is a After Pluginssssssssssss".$result;
+    // } 
+    // public function beforeGetName( \Magento\Catalog\Model\Product $subject, $result)
+    // {
+    //  return "This is a Before Pluginssssssssssss".$result;
+    // } 
+}
